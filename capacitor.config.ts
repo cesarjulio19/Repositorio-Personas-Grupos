30db955de20aeb5bc202376f2b41a1295ee98943
cesarjulio19 <cesar.loky19@gmail.com>
2024-10-18 14:29:39 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Repositorio-Personas-Grupos',
+  webDir: 'www'
+};
+
+export default config;
